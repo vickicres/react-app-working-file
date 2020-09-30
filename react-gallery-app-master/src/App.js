1fd660c417394ab7703bf7189fb429113c316236
vickicres <66817698+vickicres@users.noreply.github.com>
2020-09-30 11:09:27 -0700

    Updated App.js

--- react-gallery-app-master/src/App.js
+++ react-gallery-app-master/src/App.js
@@ -29,21 +29,20 @@ class App extends Component {
 
    // Mount components for the search function and the defaults
    componentDidMount() {
+    this.performSearch();
     this.performSearch('cats');
     this.performSearch('dogs');
     this.performSearch('birds');
-    this.performSearch();
   }
 
-  // Perform the requested search or display the default images when clicked
+  // Get data from API request when user try to search images
   performSearch = (query = 'cats') =>{
     try {
-      this.setState({ loading: true });
-      
+      this.setState({ loading: true })
+
       axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apikey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
         .then(response => {
           if (query === 'cats') {
-  
             this.setState({
               cats: response.data.photos.photo,
               loading:false
@@ -81,6 +80,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className= "container" >
+          {/* add search form */}
           <SearchForm onSearch={this.performSearch} />
           <Navigation />
      
@@ -90,6 +90,7 @@ class App extends Component {
             <Route path="/dogs" render = { () => (this.state.loading) ? <p>Loading...</p> : <ImageContainer data = {this.state.dogs}/>  }  />
             <Route path="/birds" render = { () => (this.state.loading) ? <p>Loading...</p> : <ImageContainer data = {this.state.birds}/> } /> 
             <Route path="/search/:query" render = {() => (this.state.loading) ? <p>Loading...</p> : <ImageContainer data = {this.state.search}/> } />
+            {/* route not found */}
             <Route component={ Error } />
           </Switch>
 
